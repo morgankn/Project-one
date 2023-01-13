c35313a7c5e8e83915366cd9633489112a9e29d0
morgankn <118085617+morgankn@users.noreply.github.com>
2023-01-13 13:17:15 -0500

    added more javascript

--- assets/script.js
+++ assets/script.js
@@ -24,83 +24,49 @@ function muscleGroups() {
   for (var i = 0; i < muscle.length; i++);
   console.log(muscleGroups);
 }
-var muscle = selectedExercise[Math.floor(Math.random() * selectedExercise.length)];
-fetch("https://api.api-ninjas.com/v1/exercises?muscle=" + muscle, {
-  method: "GET",
-  headers: { "X-Api-Key": "GDPoj1GGfvpCVpqAi3UAYg==YnZXl7SnTbs8tAMJ" },
-})
-  .then(function (result) {
-    return result.json();
+function fetchExercises(){
+  var muscle = selectedExercise[Math.floor(Math.random() * selectedExercise.length)];
+  fetch("https://api.api-ninjas.com/v1/exercises?muscle=" + muscle, {
+    method: "GET",
+    headers: { "X-Api-Key": "GDPoj1GGfvpCVpqAi3UAYg==YnZXl7SnTbs8tAMJ" },
   })
-  .then(function (data) {
-    console.log(data[0], data[1], data[2], data[4]);
-  })
-
-  .catch(function (jqXHR) {
-    console.error("Error: ", jqXHR);
-  });
+    .then(function (result) {
+      return result.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      displayExercise(data)
+    })
+  
+    .catch(function (jqXHR) {
+      console.error("Error: ", jqXHR);
+    })
+};
 
 function chooseMuscle() {
   console.log(data.list[0].muscle);
 }
 
 function displayExercise(data){
-  var exerciseOneEl = document.querySelector('#exercise-one');
-  var titleOne = document.createElement('h3');
-  titleOne.textContent = data[0].name;
-  exerciseOneEl.appendChild(titleOne);
-}
+  // empty the element 
+  for(var i =0; i <= 3; i ++ ){
+    var exerciseOneEl = document.querySelector('#exercise-one');
+    var titleOne = document.createElement('h3');
+    titleOne.textContent = data[i].name;
+    exerciseOneEl.appendChild(titleOne);
+    var type =document.createElement('h4');
+    type.textContent = "Type: " + data[i].type;
+    exerciseOneEl.appendChild(type);
+    var instructions = document.createElement('p');
+    type.textContent = "Instrctions: " + data[i].instructions;
+    exerciseOneEl.appendChild(instructions);
+  }
+};
 
 
-btn.addEventListener('click', displayExercise);
+btn.addEventListener('click', fetchExercises);
 
-// api url
-// const api_url =
-// 	'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle';
 
-// // Defining async function
-// async function getapi(url) {
-	
-// 	// Storing response
-// 	const response = await fetch(url);
-	
-// 	// Storing data in form of JSON
-// 	var data = await response.json();
-// 	console.log(data);
-// 	if (response) {
-// 		hideloader();
-// 	}
-// 	show(data);
-// }
-// // Calling that async function
-// getapi(api_url);
-
-// // Function to hide the loader
-// function hideloader() {
-// 	document.getElementById('loading').style.display = 'none';
-// }
-// // Function to define innerHTML for HTML table
-// function show(data) {
-// 	let tab =
-// 		`<tr>
-// 		<th>Name</th>
-// 		<th>muscle</th>
-// 		<th>Position</th>
-// 		<th>Salary</th>
-// 		</tr>`;
-	
-// 	// Loop to access all rows
-// 	for (let r of data.list) {
-// 		tab += `<tr>
-// 	<td>${r.name} </td>
-// 	<td>${r.office}</td>
-// 	<td>${r.position}</td>
-// 	<td>${r.salary}</td>		
-// </tr>`;
-// 	}
-// 	// Setting innerHTML as tab variable
-// 	document.getElementById("employees").innerHTML = tab;
-// }
 
 // // var newDiv = document.createElement('div')
 // // var newContent = document.createTextNode("Hi there and greetings!");
