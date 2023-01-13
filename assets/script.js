ef4eeb184ef8eb2e9334c8d9c22a30f09d1d09c7
morgankn <118085617+morgankn@users.noreply.github.com>
2023-01-12 21:24:03 -0500

    added elements in javascript

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+var btn = document.querySelector('.button');
+
+
 var selectedExercise = [
   "abdominals",
   "abductors",
@@ -21,8 +24,7 @@ function muscleGroups() {
   for (var i = 0; i < muscle.length; i++);
   console.log(muscleGroups);
 }
-var muscle =
-  selectedExercise[Math.floor(Math.random() * selectedExercise.length)];
+var muscle = selectedExercise[Math.floor(Math.random() * selectedExercise.length)];
 fetch("https://api.api-ninjas.com/v1/exercises?muscle=" + muscle, {
   method: "GET",
   headers: { "X-Api-Key": "GDPoj1GGfvpCVpqAi3UAYg==YnZXl7SnTbs8tAMJ" },
@@ -42,8 +44,71 @@ function chooseMuscle() {
   console.log(data.list[0].muscle);
 }
 
-// Array of upperbody, Lowerbody, and Smaller Muscles (For Loop)
-// Array inside of an Array, Random inside and Array?
-// Append to container-input
+function displayExercise(data){
+  var exerciseOneEl = document.querySelector('#exercise-one');
+  var titleOne = document.createElement('h3');
+  titleOne.textContent = data[0].name;
+  exerciseOneEl.appendChild(titleOne);
+}
 
-// 03-ACT,27- README
+
+btn.addEventListener('click', displayExercise);
+
+// api url
+// const api_url =
+// 	'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle';
+
+// // Defining async function
+// async function getapi(url) {
+	
+// 	// Storing response
+// 	const response = await fetch(url);
+	
+// 	// Storing data in form of JSON
+// 	var data = await response.json();
+// 	console.log(data);
+// 	if (response) {
+// 		hideloader();
+// 	}
+// 	show(data);
+// }
+// // Calling that async function
+// getapi(api_url);
+
+// // Function to hide the loader
+// function hideloader() {
+// 	document.getElementById('loading').style.display = 'none';
+// }
+// // Function to define innerHTML for HTML table
+// function show(data) {
+// 	let tab =
+// 		`<tr>
+// 		<th>Name</th>
+// 		<th>muscle</th>
+// 		<th>Position</th>
+// 		<th>Salary</th>
+// 		</tr>`;
+	
+// 	// Loop to access all rows
+// 	for (let r of data.list) {
+// 		tab += `<tr>
+// 	<td>${r.name} </td>
+// 	<td>${r.office}</td>
+// 	<td>${r.position}</td>
+// 	<td>${r.salary}</td>		
+// </tr>`;
+// 	}
+// 	// Setting innerHTML as tab variable
+// 	document.getElementById("employees").innerHTML = tab;
+// }
+
+// // var newDiv = document.createElement('div')
+// // var newContent = document.createTextNode("Hi there and greetings!");
+// // newDiv.appendChild(newContent);
+
+// // selectedExercise
+
+
+
+
+// // 03-ACT,27- README
